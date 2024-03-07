0b6f0d9d998f12d84d46e854256076adb8376297
Qubikq <frame.w@yandex.com>
2024-03-07 14:17:39 +0300

    static method for classes
    Никита обьяснил как сделать, спасибо НИКИТОСИК

--- classes.py
+++ classes.py
@@ -15,19 +15,34 @@ class MainCharacter:
 
 class Warrior(MainCharacter):
 
-    def __init__(self, name, age, weapon_type=weapon.weapon_bd.weapon_types[0].name, spec_name='Воин', hp=50, armor=5, speed=10, damage=weapon.weapon_bd.weapon_types[0].damage):
+    def __init__(self, name, age, weapon_type=weapon.weapon_bd.weapon_types[0].name, spec_name='Воин', hp=50,
+                 armor=5, speed=10, damage=weapon.weapon_bd.weapon_types[0].damage):
         super().__init__(name, age, weapon_type, spec_name, hp, armor, speed, damage)
 
 
 class Mage(MainCharacter):
 
-    def __init__(self, name, age, weapon_type=weapon.weapon_bd.mage_weapon_types[0].name, spec_name='Маг', hp=30, armor=2, speed=15, damage=weapon.weapon_bd.mage_weapon_types[0].damage):
+    def __init__(self, name, age, weapon_type=weapon.weapon_bd.mage_weapon_types[0].name, spec_name='Маг', hp=30,
+                 armor=2, speed=15, damage=weapon.weapon_bd.mage_weapon_types[0].damage):
         super().__init__(name, age, weapon_type, spec_name, hp, armor, speed, damage)
 
 
 class Archer(MainCharacter):
 
-    def __init__(self, name, age, weapon_type=weapon.weapon_bd.archer_weapon_types[0].name, spec_name='Лучник', hp=35, armor=4, speed=23, damage=weapon.weapon_bd.archer_weapon_types[0].damage):
+    def __init__(self, name, age, weapon_type=weapon.weapon_bd.archer_weapon_types[0].name, spec_name='Лучник',
+                 hp=35, armor=4, speed=23, damage=weapon.weapon_bd.archer_weapon_types[0].damage):
         super().__init__(name, age, weapon_type, spec_name, hp, armor, speed, damage)
 
 
+class Heroes(Warrior, Mage, Archer):
+    @staticmethod
+    def make_warrior(name, age):
+        return Warrior(name, age)
+
+    @staticmethod
+    def make_mage(name, age):
+        return Mage(name, age)
+
+    @staticmethod
+    def make_archer(name, age):
+        return Mage(name, age)
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import classes
 
-
 Hero = None
+
+
+def hero(chos_hero=None):
+    name_hero = input('Имя вашего персонажа: ')
+    age_hero = int(input('Возраст вашего персонажа: '))
+    return getattr(classes.Heroes, chos_hero)(name=name_hero, age=age_hero)
+
+
 while True:
     print(' Добро пожаловать в меню создания перосонажа! \n', 'Что вы хотите сделать?\n')
     print(' 1 - Создать персонажа \n', '2 - О персонаже \n', '3 - Редактор класса \n',
@@ -11,23 +18,24 @@ while True:
     match chose:
         case 1:
             while True:
+
                 print('Выберите класс для персонажа: \n 1. Воин \n 2. Маг \n 3. Лучник \n 9. Выход \n')
                 chose = int(input('Выберите действие: '))
                 match chose:
                     case 1:
-                        Hero = classes.Warrior(input('Имя вашего персонажа: '), input('Возраст вашего персонажа: '))
+                        Hero = hero('make_warrior')
                         break
                     case 2:
-                        Hero = classes.Mage(input('Имя вашего персонажа: '), input('Возраст вашего персонажа: '))
+                        Hero = hero('make_mage')
                         break
                     case 3:
-                        Hero = classes.Archer(input('Имя вашего персонажа: '), input('Возраст вашего персонажа: '))
+                        Hero = hero('make_archer')
                         break
                     case 9:
                         break
 
         case 2:
             print(
-                f'Имя - {Hero.name} \n Возраст - {Hero.age} \n Оружие - {Hero.weapon} \n Класс - {Hero.spec_name} \n --------------------- \n Здоровье - {Hero.hp} \n Броня - {Hero.armor} \n Скорость - {Hero.speed} \n Урон - {Hero.damage} \n ')
+                f'\n Имя - {Hero.name} \n Возраст - {Hero.age} \n Оружие - {Hero.weapon} \n Класс - {Hero.spec_name} \n --------------------- \n Здоровье - {Hero.hp} \n Броня - {Hero.armor} \n Скорость - {Hero.speed} \n Урон - {Hero.damage} \n ')
         case 9:
             break
